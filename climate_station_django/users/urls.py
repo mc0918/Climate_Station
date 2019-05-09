a0c1be6c6c2835f2f88829c84e9fba700468bfab
mc0918 <mcampbell0918@gmail.com>
2019-05-08 23:51:50 -0500

    created react and django frameworks

---
+++ climate_station_django/users/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'', views.home, name='home'),
+    # url(r'^detail/$', views.detail, name='detail'),
+    # url(r'^add/$', views.add, name='add'),
+]
