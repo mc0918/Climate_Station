4c802d54b7a2ab48e4e6810d20f2e0c0669ca03a
mc0918 <mcampbell0918@gmail.com>
2019-05-09 00:05:24 -0500

    cleaned frameworks

--- README.md
+++
@@ -1,2 +0,0 @@
-# Climate_Station
-Analyze and visualize data gathered from Northwestern University's climate station
--- climate_station_django/manage.py
+++ climate_station_django/manage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# using virtualenv gitenv
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
