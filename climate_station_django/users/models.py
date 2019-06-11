ebe0bdbf51ac7e179502b05a36eb358050542459
mc0918 <mcampbell0918@gmail.com>
2019-06-11 14:22:50 -0500

    added saved data model

--- climate_station_django/users/models.py
+++ climate_station_django/users/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Users(models.Users):
     user_name = models.CharField(max_length=200)
     join_date = models.DateTimeField('date joined')
+
+class Saved_Data(models.Saved_Data):
+    Record = models.IntegerField(default=0)
+    Record_Date = models.CharField(max_length=200)
