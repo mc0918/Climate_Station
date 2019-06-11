7951d2b262a455665ac587395a92e4ebd198ab6c
mc0918 <mcampbell0918@gmail.com>
2019-06-11 15:22:02 -0500

    migrated Users and Saved_Data models

--- climate_station_django/users/models.py
+++ climate_station_django/users/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Users(models.Users):
+class Users(models.Model):
     user_name = models.CharField(max_length=200)
     join_date = models.DateTimeField('date joined')
 
-class Saved_Data(models.Saved_Data):
+class Saved_Data(models.Model):
     Record = models.IntegerField(default=0)
     Record_Date = models.CharField(max_length=200)
