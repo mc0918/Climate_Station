ddd68dcdf62c11ff380a16b61458f6b9bf0b249b
mc0918 <mcampbell0918@gmail.com>
2019-06-11 16:07:21 -0500

    added methods to models, TODO: create admin user

--- climate_station_django/users/models.py
+++ climate_station_django/users/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Users(models.Model):
     user_name = models.CharField(max_length=200)
     join_date = models.DateTimeField('date joined')
+    def __str__(self):
+        return self.user_name
+    def length_joined(self):
+        return self.join_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Saved_Data(models.Model):
     Record = models.IntegerField(default=0)
     Record_Date = models.CharField(max_length=200)
+    def __str__(self):
+        return self.Record
