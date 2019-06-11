c7e1fa1d5c481a2a3af532f23a24cf7bd0afd27f
mc0918 <mcampbell0918@gmail.com>
2019-06-11 14:14:46 -0500

    added user model

--- climate_station_django/users/models.py
+++ climate_station_django/users/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Users):
+    user_name = models.CharField(max_length=200)
+    join_date = models.DateTimeField('date joined')
